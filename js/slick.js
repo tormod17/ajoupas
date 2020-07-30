8358ade478b7bf313dfda004d34a5019cf8b3698
Tormod Smith <tormod.smith@yieldify.com>
2020-07-31 00:14:33 +0100

    change contact details

--- js/slick.js
+++ js/slick.js
@@ -1,28 +1,22 @@
   var affiliateLinks = [
     "https://www.amazon.co.uk/gp/product/B00DTXYCOU/ref=as_li_tl?ie=UTF8&camp=1634&creative=6738&creativeASIN=B00DTXYCOU&linkCode=as2&tag=clickaton-21&linkId=7a965e18d33ba53ca5613eef3cf53f09",
-    "https://www.amazon.co.uk/gp/product/B071CZ73J2/ref=as_li_tl?ie=UTF8&camp=1634&creative=6738&creativeASIN=B071CZ73J2&linkCode=as2&tag=clickaton-21&linkId=db2f0889a8b0cecb1aebc41bab124dd9",
     "https://www.amazon.co.uk/Bose-QuietComfort-Wireless-Headphones-Cancelling-Black/dp/B0756CYWWD/ref=as_li_ss_il?crid=3T2Z7DIN6M2Y3&keywords=noise+cancelling+headphones&qid=1553976988&rnid=419152031&s=gateway&sprefix=noise+,aps,134&sr=8-7&linkCode=li1&tag=clickaton-21&linkId=aba6b5ec3d4aac6c9c61addcfcf228a8&language=en_GB",
     "https://www.amazon.co.uk/gp/product/B016WZ15YG/ref=as_li_tl?ie=UTF8&camp=1634&creative=6738&creativeASIN=B016WZ15YG&linkCode=as2&tag=clickaton-21&linkId=5b62feda3e1e5c2c3537abd26764311b",
-    "https://www.amazon.co.uk/gp/product/B078Q5TTGF/ref=as_li_tl?ie=UTF8&camp=1634&creative=6738&creativeASIN=B078Q5TTGF&linkCode=as2&tag=clickaton-21&linkId=8f742fe2cfa826438041785013f7d801",
     "https://www.amazon.co.uk/gp/product/B01CL7YRDU/ref=as_li_tl?ie=UTF8&camp=1634&creative=6738&creativeASIN=B01CL7YRDU&linkCode=as2&tag=clickaton-21&linkId=1ba094711de9338f42680466929243df",
     "https://www.amazon.co.uk/gp/product/B071RSHMJN/ref=as_li_tl?ie=UTF8&camp=1634&creative=6738&creativeASIN=B071RSHMJN&linkCode=as2&tag=clickaton-21&linkId=10c16175c5043b9d6ce611816779c06e"
   ];
 
   var imageSrcs = [
     "http://ws-eu.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=GB&ASIN=B00DTXYCOU&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL250_&tag=clickaton-21",
-    "http://ws-eu.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=GB&ASIN=B071CZ73J2&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL160_&tag=clickaton-21",
     "http://ws-eu.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B0756CYWWD&Format=_SL110_&ID=AsinImage&MarketPlace=GB&ServiceVersion=20070822&WS=1&tag=clickaton-21&language=en_GB",
     "http://ws-eu.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=GB&ASIN=B016WZ15YG&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL110_&tag=clickaton-21",
-    "http://ws-eu.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=GB&ASIN=B078Q5TTGF&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL110_&tag=clickaton-21",
     "http://ws-eu.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=GB&ASIN=B01CL7YRDU&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL110_&tag=clickaton-21",
     "http://ws-eu.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=GB&ASIN=B071RSHMJN&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL110_&tag=clickaton-21"
   ];
   var pixels = [
     "http://ir-uk.amazon-adsystem.com/e/ir?t=clickaton-21&l=am2&o=2&a=B00DTXYCOU",
-    "http://ir-uk.amazon-adsystem.com/e/ir?t=clickaton-21&l=am2&o=2&a=B071CZ73J2",
     "https://ir-uk.amazon-adsystem.com/e/ir?t=clickaton-21&language=en_GB&l=li1&o=2&a=B0756CYWWD",
     "http://ir-uk.amazon-adsystem.com/e/ir?t=clickaton-21&l=am2&o=2&a=B016WZ15YG",
-    "http://ir-uk.amazon-adsystem.com/e/ir?t=clickaton-21&l=am2&o=2&a=B078Q5TTGF",
     "http://ir-uk.amazon-adsystem.com/e/ir?t=clickaton-21&l=am2&o=2&a=B01CL7YRDU",
     "http://ir-uk.amazon-adsystem.com/e/ir?t=clickaton-21&l=am2&o=2&a=B071RSHMJN"
   ];
@@ -52,7 +46,7 @@ function createAffiliateSlide(slideData) {
   );
   pixel.attr("src", slideData.pixel);
   var button = $(
-    '<div class="buy-button"><p>BUY NOW ON</p><img width="60" src="img/amazon_logo.png" /></div>'
+    '<div class="buy-button"><p>BUY NOW ON</p><img width="50" src="img/amazon_logo.png" /></div>'
   );
   anchorWrapper.append(image, pixel, button);
   slide.append(anchorWrapper);
@@ -74,10 +68,10 @@ function createAndAppendSlideShow(slides, callback) {
 
 function slickSlideShowInit() {
   $(".shop-slide-show").slick({
-    slidesToShow: 6,
+    slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 2000
+    autoplaySpeed: 8000
   });
 }
 
